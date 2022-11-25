238f6ff775ae40b45bfb1e6488f317810ecb4e71
Daniel Klein <daniel.klein@develit.io>
2022-11-25 12:48:52 +0100

    fix: spacing

--- lib/instruction/widgets/instruction_texts.dart
+++ lib/instruction/widgets/instruction_texts.dart
@@ -13,19 +13,25 @@ class InstructionTexts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final padding = screenWidth > 768 ? screenWidth * 0.2 : 16;
     return Column(
       children: texts.map((t) {
         return Container(
           alignment: Alignment.center,
           padding: EdgeInsets.symmetric(
             vertical: 8,
-            horizontal: MediaQuery.of(context).size.width * 0.2,
+            horizontal: padding.toDouble(),
           ),
           child: Text(
             t,
             overflow: TextOverflow.clip,
-            style: const TextStyle(
-              fontSize: 20,
+            style: TextStyle(
+              fontSize: screenWidth > 1078
+                  ? 22
+                  : screenWidth > 768
+                      ? 20
+                      : 18,
             ),
           ),
         );
